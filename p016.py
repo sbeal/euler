05cc0f372afde320615ebd1848f885041748cdcc
Steve Beal <stevewbeal@gmail.com>
2014-02-21 18:00:48 -0500

    added another method to p016.py

--- p016.py
+++ p016.py
@@ -5,7 +5,14 @@
 # 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 # What is the sum of the digits of the number 2^1000?
 
-# easy way using string conversion
-
 val = 2**1000
+
+# "easy" way using string conversion
 print sum(int(c) for c in str(val))
+
+# another way by grabbing digits via modulus (slower if string is long)
+s = 0
+while val > 0:
+    val, r = divmod(val, 10)
+    s += r
+print s
