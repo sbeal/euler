34c497e9f388063180fe3953f870ed20fed8c641
Steve Beal <stevewbeal@gmail.com>
2015-01-11 10:54:25 -0500

    Add solutions to problems 26 and 27, update gitignore

---
+++ p026.py
@@ -0,0 +1,64 @@
+# Steve Beal
+# Project Euler problem 26 solution
+# 1/11/15
+
+# A unit fraction contains 1 in the numerator. The decimal representation
+# of the unit fractions with denominators 2 to 10 are given:
+# 1/2 = 0.5
+# 1/3 = 0.(3)
+# 1/4 = 0.25
+# 1/5 = 0.2
+# 1/6 = 0.1(6)
+# 1/7 = 0.(142857)
+# 1/8 = 0.125
+# 1/9 = 0.(1)
+# 1/10 = 0.1
+
+# Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle.
+# It can be seen that 1/7 has a 6-digit recurring cycle.
+
+# Find the value of d < 1000 for which 1/d contains the longest recurring
+# cycle in its decimal fraction part.
+
+def get_cycle_len(n):
+    remainders = { 1: 0 }
+    cycle_len = 0
+    i, r, cycle_len = 0, 1, 0
+    while True:
+        # the next remainder is 10 times the previous one, mod divisor
+        r = 10*r % n
+
+        # if remainder is 0, we've divided evenly and don't have a cycle
+        if r == 0:
+            cycle_len = 0
+            break
+        else:
+            # if we've seen this before, it's the end of the cycle
+            if r in remainders:
+                cycle_len = i + 1 - remainders[r]
+                break
+            # otherwise, the cycle continues
+            else:
+                remainders[r] = i + 1
+        i+=1
+    return cycle_len
+
+def longest_reciprocal_cycle(d):
+    # start from the bigger end, since it's more likely to have long cycles
+    max_cycle_len = 0
+    max_cycle_num = 1001
+    for num in range(d, 1, -1):
+        # max cycle len is num-1, so stop if it's already bigger than any of the
+        # remaining numbers
+        if max_cycle_len > num:
+            break
+
+        cycle = get_cycle_len(num)
+        if cycle > max_cycle_len:
+            max_cycle_len = cycle
+            max_cycle_num = num
+
+    return (max_cycle_num, max_cycle_len)
+
+
+print(longest_reciprocal_cycle(1000))
---
+++ p027.py
@@ -0,0 +1,45 @@
+# Steve Beal
+# Project Euler problem 27 solution
+# 1/11/15
+
+# Euler discovered the remarkable quadratic formula n^2 + n + 41
+
+# It turns out that the formula will produce 40 primes for the consecutive
+# values n = 0 to 39. However, when n = 40, 40^2 + 40 + 41 = 40(40 + 1) + 41 is
+# divisible by 41, and certainly when n = 41, 41^2 + 41 + 41 is clearly
+# divisible by 41.
+
+# The incredible formula n^2 - 79n + 1601 was discovered, which produces
+# 80 primes for the consecutive values n = 0 to 79. The product of the
+# coefficients, -79 and 1601, is -126479.
+
+# Considering quadratics of the form n^2 + an + b, where |a|, |b| < 1000
+# where |n| is the modulus/absolute value of n, e.g. |11| = 11 and |-4| = 4
+
+# Find the product of the coefficients, a and b, for the quadratic expression
+# that produces the maximum number of primes for consecutive values of n,
+# starting with n = 0.
+
+from utils import is_prime
+
+def num_consecutive_primes(a, b):
+    n = 0
+    val = n**2 + a*n + b
+    num_primes = 0
+    while is_prime(val):
+        num_primes += 1
+        n += 1
+        val = n**2 + a*n + b
+    return num_primes
+
+max_primes = -1
+prod = 0
+for a in range(-999, 1000):
+    for b in range(-999, 1000):
+        num_primes = num_consecutive_primes(a, b)
+        if num_primes > max_primes:
+            max_primes = num_primes
+            prod = a*b
+
+
+print(max_primes, prod)
